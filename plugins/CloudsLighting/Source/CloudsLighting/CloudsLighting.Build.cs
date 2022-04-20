40c815cbefd7d3797fd6f928de21f8778a0f9a2f
Stratosferaa <88853813+Stratosferaa@users.noreply.github.com>
2022-04-21 02:58:22 +0300

    12
    12

---
+++ plugins/CloudsLighting/Source/CloudsLighting/CloudsLighting.Build.cs
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 by Lukerrr
+
+using UnrealBuildTool;
+
+public class CloudsLighting : ModuleRules
+{
+	public CloudsLighting(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+			
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"RenderCore",
+				"RHI",
+			}
+			);
+	}
+}
